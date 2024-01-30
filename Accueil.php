6054595a583cb72c6bbe25c2c4bd3459434beaac
ProjMax <158173639+ProjMax@users.noreply.github.com>
2024-01-30 13:59:46 +0000

    Ajout page Accueil

---
+++ Accueil.php
@@ -0,0 +1,33 @@
+<?php
+// definir les sessions
+//         partie chiffrement
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Accueil</title>
+</head>
+<body class="container">
+    <h1>Bienvenue sur le site du Demineur en Ligne !</h1>
+    <!-- <p>Merci de vous identifier pour accéder à l'application.</p> -->
+    <!-- Affichage du message d'erreur -->
+    <!-- Code saisit invalide | pseudo deja empreinter | -->
+    
+    <div>
+        <label for="inCreator">Pseudo createur: </label>
+        <input class="form-control" id="inCreator" name="Creator" type="text" placeholder="identifiant createur">
+        <button class="btn btn-primary" type="submit" id="btnCreate">Creation</button>
+    </div>
+    <div>
+        <label for="inJoiner">Pseudo joueur 2 : </label>
+        <input class="form-control" id="inJoiner" name="Joiner" type="text" placeholder="Veuilliez saisir le code de la partie">
+        <button class="btn btn-warning" type="reset" id="btnJoin">Rejoindre</button>
+    </div>
+    
+</body>
+</html>
