28e684911757f6a0dbbd0f8f76ce0d47bdcc50ce
ProjMax <158173639+ProjMax@users.noreply.github.com>
2024-02-07 15:48:31 +0100

    Modif page accueil

--- page/Accueil.php
+++ page/Accueil.php
@@ -9,24 +9,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Accueil</title>
 </head>
-<body class="container">
-    <h1>Bienvenue sur le site du Demineur en ligne le meilleur du monde !</h1>
+<body>
+    <div class="container">
+    <h1><center>Bienvenue sur le site du Demineur en ligne !</center></h1>
     <!-- <p>Merci de vous identifier pour accéder à l'application.</p> -->
     <!-- Affichage du message d'erreur -->
     <!-- Code saisit invalide | pseudo deja empreinter | -->
-    
+    </div>
     <div>
         <label for="inCreator">Pseudo createur: </label>
         <input class="form-control" id="inCreator" name="Creator" type="text" placeholder="identifiant createur">
         <button class="btn btn-primary" type="submit" id="btnCreate">Creation</button>
+        
     </div>
     <div>
         <label for="inJoiner">Pseudo joueur 2 : </label>
         <input class="form-control" id="inJoiner" name="Joiner" type="text" placeholder="Veuilliez saisir le code de la partie">
         <button class="btn btn-warning" type="reset" id="btnJoin">Rejoindre</button>
+        <h3>Un lien pour déclencher un script JavaScript</h3>
+        <a href="javascript:alert('Le code a bien été générer');">Generation</a>
     </div>
     
 </body>
