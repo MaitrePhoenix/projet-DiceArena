fba32874d8f2a423d5176169de760c128dfd2d93
ProjMax <darnauxm@3il.fr>
2024-02-08 17:23:11 +0100

    Modif accueil connexion

--- page/accueil.php
+++ page/accueil.php
@@ -8,6 +8,15 @@
     // Redirection vers la page avec le code unique dans l'URL
     // header("Location: jeu.php?code=$code_unique");
     // exit;
+
+// Utilisez include si la non-disponibilité de inc_headers.php n'est pas critique
+include('includes/inc_headers.php');
+
+//if(!empty($_GET["msg"]))
+//$msg=""
+$erreurMessIdent = "Erreur d'identification. Veuillez vérifier vos informations.";
+// Utilisez require si la non-disponibilité de inc_headers.php doit être traitée comme une erreur fatale
+// require('includes/inc_headers.php');
 ?>
 
 <!DOCTYPE html>
@@ -21,24 +30,37 @@
     <title>Accueil</title>
 </head>
 
-<body>
-    <div class="container">
+<body style="background-color: #f2f2f2">
+    <div >
         <h1><center>Bienvenue sur le site DiceArena !</center></h1>
         <!-- <p>Merci de vous identifier pour accéder à l'application.</p> -->
         <!-- Affichage du message d'erreur -->
         <!-- Code saisit invalide | pseudo deja empreinter | -->
         
-        <div>
+        <div class="center_container" style="background-color: #fff;
+                                    padding: 30px;
+                                    border-radius: 8px;
+                                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+
+            <?php   
+            //if msg!=""
+            if (isset($_GET['erreur']) && $_GET['erreur'] == 1) {
+                echo '<p style="color: red;"><mark>'. $erreurMessIdent .'</mark></p>';
+            }
+            ?>
+
             <!-- <label for="inCreator">Pseudo createur: </label> -->
             <!-- <input class="form-control" id="inCreation" name="Creation" type="text" placeholder="identifiant createur"> -->
-            <button class="btn btn-primary" type="submit" id="btnCreate" action="traiterCreation.php">Creation</button>
+            <button class="btn btn-primary" type="submit" id="btnCreate" action="traiterCreation.php" style="margin-right:10px;">Creer</button>
+            <button class="btn btn-warning" type="reset" id="btnJoin" action="traiterJoin.php" style="margin-right:3px;">Rejoindre</button>
+            <!-- <label for="inCode">Code : </label> -->
+            <input class="form-control input-accueil" id="inCode" name="Code" type="text" placeholder="Saisir le code" 
+                    style="width: 150px;" maxlength="12" >
+        </div>
+        <!-- <div class="margin-top-acceuil">
             
-        </div>
-        <div class="margin-text">
-            <label for="inCode">Code : </label>
-            <input class="form-control input-accueil" id="inCode" name="Code" type="text" placeholder="Veuilliez saisir le code de la partie">
-            <button class="btn btn-warning" type="reset" id="btnJoin" action="traiterJoin.php">Rejoindre</button>
-        </div>
+            
+        </div> -->
     </div>
 </body>
 </html>
---
+++ traitement/traiterIdentification.php
@@ -0,0 +1,104 @@
+<?php
+    //Version Hash
+    session_start(); // "premiere instruction; pas de texte, ; entete de communication"
+    
+    require_once "includes/inc_connexionBase.php";
+    //----------------------------------------
+    // Un peu de tracage des valeurs recues
+    //----------------------------------------
+
+    echo '$_REQUEST : ';
+    var_dump($_REQUEST);
+    echo "<br>";
+
+    echo '$_SERVER[\"REQUEST_METHOD\"] : ';
+    var_dump($_SERVER['REQUEST_METHOD']);
+    echo "<br>";
+
+    echo '$_GET : ';
+    var_dump($_GET);
+    echo "<br>";
+
+    echo '$_POST : ';
+    var_dump($_POST);
+    echo "<br>";
+
+    //exit() //Pour voir vraiment ce qui est donner à mon formulaire, 
+            // aide a donner les parametre à mes variables que je créer ici 
+            //en gros on voit toutes les var partager
+
+    //----------------------------------------------------------------------------------
+    // Traitement des valeurs
+    // Attention : choix de passer par $_REQUEST par facilité,
+    //             pas nécessairement une bonne pratique    
+    //----------------------------------------------------------------------------------
+
+
+    // on passe par des variables plus rapides � �crire...
+    $login = $_REQUEST['login'];
+    $pass  = $_REQUEST['pass'];
+
+    var_dump($_REQUEST);
+
+    $text_request = "SELECT login, pass ";
+    $text_request.= "FROM joueur ";
+    $text_request.= "WHERE login=:log;";
+
+    $requete = $cnx->prepare($text_request);
+    $requete ->bindParam(":log",$login);
+
+    $requete->execute();
+
+    $tabRes = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    //Partie debug
+    //var_dump($tabRes);
+    //exit();
+
+    //Si on ne retourne pas une ligne -> il y a un soucis
+    if(count($tabRes)!=1){
+        $_SESSION["login"] = "";
+        // $_SESSION["droits"] = "";
+        header("location:connexion.php?erreur=2");//Modifier avec droits non existants
+        exit();
+    }
+
+    //Si on arrive ici c'est que le login existe
+
+    //$droits = $tabRes[0]["droits"];
+    $passHash = $tabRes[0]["pass"];
+    //Est ce que le pass est correct ?
+    if(password_verify($pass,$passHash)==false){
+        die("Erreur L71");
+    }
+
+    //Placer en session : le login et les droits
+    $_SESSION["login"] = $login;
+    //$_SESSION["droits"] = $droits;
+
+    // if ($droits=="admin"){
+    //     // rediriger vers la page d'admin
+    //     header("location:pageAdmin.php");
+    //     exit(); // header ne provoque pas l'arret du script
+    // }
+    // elseif ($droits=="etudiant"){
+    //    // rediriger vers la page d'affichage du profil de l'etudiant
+    //     header("location:pageProfil.php");//?nom=zoe
+    //     exit(); // header ne provoque pas l'arret du script
+    // }
+    if (empty($login) || empty($pass)){
+	// Pas re�ues, ou "vide" : on sort
+    //if (empty($login) || empty($pass)){
+        header("location:connexion.php?erreur=1");
+        exit(); // header ne provoque pas l'arret du script
+    //}
+    }
+    else 
+    {
+        header("location:accueil.php");//?nom=mael
+        //exit(); // header ne provoque pas l'arret du script
+    }
+    
+
+    // si pas connu : rediriger sur la page de co (connexion.php)
+    header("location:connexion.php?erreur=1");
