0db68c0636cd297375b6e6926f8c1c2d3c2d5fd7
Maël Moulin <maelmoulinveyrac@gmail.com>
2024-02-14 15:43:24 +0100

    test (temporaire) acces base

--- includes/inc_connexionBase.php
+++ includes/inc_connexionBase.php
@@ -1,5 +1,6 @@
 <?php
 
+$cnx=null;
     try{
         //$cnx = new PDO("mysql:host=mysql;dbname=tpweb","tpweb","TyTy1234");
         //Ne pas taper le port de l'hote 127.0.0.0
@@ -7,7 +8,6 @@
         $cnx = new PDO("mysql:host=localhost;dbname=DiceArena","tpweb","tropSimpl3");
     }catch (PDOException $e){// $e est une exception PDO
         echo $e->getMessage();
-        $cnx=null;
     }
 
     
--- script/scriptAccesBdd.php
+++ script/scriptAccesBdd.php
@@ -3,6 +3,10 @@
 require_once "includes/inc_connexionBase.php";
 
 function getJoueurById($id){
-
+    $sql = "SELECT * FROM joueur";
+    foreach ($cnx->query($sql) as $row) {
+        print $row['id'] . "\t";
+        print $row['pseudo'] . "\t";
+    }
 }
 ?>
